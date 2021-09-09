571452adfa29fa9f774744754458a122e7dc840c
Marat Khanbekov <marat.khanbekov@gmail.com>
2021-09-09 20:52:55 +0300

    Initial Commit

---
+++ gb-swiftui/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  gb-swiftui
+//
+//  Created by Marat Khanbekov on 09.09.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ gb-swiftui/gb_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  gb_swiftuiApp.swift
+//  gb-swiftui
+//
+//  Created by Marat Khanbekov on 09.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct gb_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
